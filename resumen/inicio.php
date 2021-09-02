0b976831033e9665bbfe696287d39cc2a81c7434
jacortes <jacortes6889@misena.edu.co>
2021-09-02 18:17:21 -0500

    primera version

--- resumen/inicio.php
+++ resumen/inicio.php
@@ -4,9 +4,78 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="estilo1.css">
+    <title>poo</title>
+    
 </head>
 <body>
-    
+    <h1>POO</h1>
+    <div id="div1">
+    <div id="d1">
+    <h2>CONCEPTO</h2>
+    <p>La programación orientada a objetos o POO es un paradigma 
+        de programación que usa los objetos en sus interacciones,
+        para diseñar aplicaciones y programas informáticos. Está basado en varias 
+        técnicas, incluyendo herencia, abstracción, polimorfismo y encapsulamiento.</p>
+    </div> 
+       
+    <div id="d3">
+        <div id="di1"><img src="img.jpg"></div>
+        <div id="di2"><img src="img1.png"></div>
+        <div id="di3"><img src="img2.jfif"></div>
+        <div id="di4"><img src="img3.jpg"></div>
+      
+    </div>
+    <div id="d2">
+    <h2>INTRODUCCION</h2> 
+    <p> objetos son entidades1 que tienen un determinado estado, comportamiento (método) e identidad:</p>
+<ul>
+    <li>El estado está compuesto de datos o informaciones, será uno o varios atributos a los que se habrán asignado unos valores concretos (datos).</li>
+    <li>El comportamiento está definido por los métodos o mensajes a los que sabe responder dicho objeto, es decir, qué operaciones se pueden realizar con él.</li>
+    <li>La identidad es una propiedad de un objeto que lo diferencia del resto, dicho con otras palabras, es su identificador (concepto análogo al de identificador de una variable o una constante).</li>
+</ul>
+    <p>Un objeto contiene toda la información que permite definirlo e identificarlo frente a otros objetos pertenecientes a otras clases e incluso frente a objetos de una misma clase, al poder tener valores bien diferenciados en sus atributos</p>
+    <p>Los métodos (comportamiento) y atributos (estado) están estrechamente relacionados por la propiedad de conjunto. Esta propiedad destaca que una clase requiere de métodos para poder tratar los atributos con los que cuenta</p>
+    </div>
+    </div>
+
+    <p>_____________________________________________________________________________________</p>
+    <div id="div2">
+
+    <h2 id="pi">LOS CUATRO PILARES DE LA POO</h2>
+    <div id="d4">
+    <h3>Pilar 1: Encapsulación</h3>
+    <p>La encapsulación es la característica de un lenguaje POO que permite que todo lo referente a un objeto quede aislado dentro de éste. Es decir, que todos los datos referentes a un objeto queden "encerrados" dentro de éste y sólo se puede acceder a ellos a través de los miembros que la clase proporcione (propiedades y métodos).</p>
+    </div>
+    <div id="d5">
+    <h3>Pilar 2: Abstracción</h3>
+    <p>la clase debe representar las características de la entidad hacia el mundo exterior, pero ocultando la complejidad que llevan aparejada</p>
+    </div> 
+    <div id="d7">
+    <h3>Pilar 4: Polimorfismo</h3>
+    <p>la polimorfismo es cuando varios objetos de diferentes clases, pero con una base común, se pueden usar de manera indistinta, sin tener que saber de qué clase exacta son para poder hacerlo.</p>
+    </div>
+    <div id="d6">
+    <h3>Pilar 3: Herencia</h3>
+    <p>en POO cuando una clase hereda de otra obtiene todos los rasgos que tuviese la primera y añade otros nuevos y además también puede modificar algunos de los que ha heredado.</p>
+    </div>
+   
+    </div>
+    <h2>CONCEPTOS FUNDAMENTALES</h2>
+    <ul>
+        <li><b>Clase</b>: definiciones de las propiedades y comportamiento de un tipo de objeto concreto. La instanciación es la lectura de estas definiciones y la creación de un objeto a partir de ellas.</li><br>
+        <li><b>Herencia</b>: (por ejemplo, herencia de la clase C a la clase D) Es la facilidad mediante la cual la clase D hereda en ella cada uno de los atributos y operaciones de C, como si esos atributos y operaciones hubiesen sido definidos por la misma D. Por lo tanto, puede usar los mismos métodos y variables públicas declaradas en C. Los componentes registrados como "privados" (private) también se heredan, pero como no pertenecen a la clase, se mantienen escondidos al programador y sólo pueden ser accedidos a través de otros métodos públicos. Esto es así para mantener hegemónico el ideal de OOP.</li><br>
+        <li><b>Objeto</b>: entidad provista de un conjunto de propiedades o atributos (datos) y de comportamiento o funcionalidad (métodos) los mismos que consecuentemente reaccionan a eventos. Se corresponde con los objetos reales del mundo que nos rodea, o a objetos internos del sistema (del programa). Es una instancia a una clase.</li><br>
+        <li><b>Método</b>: Algoritmo asociado a un objeto (o a una clase de objetos), cuya ejecución se desencadena tras la recepción de un "mensaje". Desde el punto de vista del comportamiento, es lo que el objeto puede hacer. Un método puede producir un cambio en las propiedades del objeto, o la generación de un "evento" con un nuevo mensaje para otro objeto del sistema.</li><br>
+        <li><b>Evento</b>: Es un suceso en el sistema (tal como una interacción del usuario con la máquina, o un mensaje enviado por un objeto). El sistema maneja el evento enviando el mensaje adecuado al objeto pertinente. También se puede definir como evento, a la reacción que puede desencadenar un objeto, es decir la acción que genera.</li><br>
+        <li><b>Mensaje</b>: una comunicación dirigida a un objeto, que le ordena que ejecute uno de sus métodos con ciertos parámetros asociados al evento que lo generó.</li><br>
+        <li><b>Propiedad o atributo</b>: contenedor de un tipo de datos asociados a un objeto (o a una clase de objetos), que hace los datos visibles desde fuera del objeto y esto se define como sus características predeterminadas, y cuyo valor puede ser alterado por la ejecución de algún método.</li><br>
+        <li><b>Estado interno</b>: es una variable que se declara privada, que puede ser únicamente accedida y alterada por un método del objeto, y que se utiliza para indicar distintas situaciones posibles para el objeto (o clase de objetos). No es visible al programador que maneja una instancia de la clase.</li><br>
+        <li><b>Componentes de un objeto</b>: atributos, identidad, relaciones y métodos.</li><br>
+        <li><b>Identificación de un objeto</b>: un objeto se representa por medio de una tabla o entidad que esté compuesta por sus atributos y funciones correspondientes.</li><br>
+    </ul>
+    <h2>BIBLIOGRAFIA</h2>
+    <a href="https://es.wikipedia.org/wiki/Programaci%C3%B3n_orientada_a_objetos">https://es.wikipedia.org/wiki/Programaci%C3%B3n_orientada_a_objetos</a>  <br> 
+    <a href="https://www.ionos.es/digitalguide/dominios/gestion-de-dominios/metodos-para-redireccionar-una-pagina-web/">https://www.ionos.es/digitalguide/dominios/gestion-de-dominios/metodos-para-redireccionar-una-pagina-web/</a>
 </body>
 </html>
